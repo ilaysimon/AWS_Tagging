4f8d4ded64baa128b8f6dfb99d9ba66b941f4e84
ilaysimon <70803336+ilaysimon@users.noreply.github.com>
2022-07-05 15:54:39 +0300

    Update tag_manual_ASG_lambda.py
    Add sns function

--- tag_manual_ASG_lambda.py
+++ tag_manual_ASG_lambda.py
@@ -2,11 +2,11 @@ import sys
 import boto3
 from botocore.exceptions import ClientError
 import logging
+import os
 
-#manual-ASG-ilay
-#ilay-ASG-No-Manual
-
-#ASG_client = boto3.client('autoscaling')
+#Get the env variables
+SNS_REGION = os.getenv('SNS_REGION')
+SNS_ARN_TOPIC = os.getenv('SNS_ARN_TOPIC')
 
 
 def check_scaling_policies(list_ASG_names,ASG_client):
@@ -20,8 +20,7 @@ def check_scaling_policies(list_ASG_names,ASG_client):
         else :
             ASG_dynamic.append(ASG_name)
     return ASG_manual
-    # return(f"ASG_manual: {ASG_manual}")
-    
+
 
 def update_tags(list_manual_ASG_names,ASG_client) :
     for ASG_name in list_manual_ASG_names:
@@ -31,21 +30,21 @@ def update_tags(list_manual_ASG_names,ASG_client) :
                 'PropagateAtLaunch': True, #Determines whether the tag is added to new instances as they are launched in the group.
                 'ResourceId': ASG_name,
                 'ResourceType': 'auto-scaling-group',
-                'Key': 'k',
-                'Value': 'k',
+                'Key': 'auto_scale_candidate',
+                'Value': 'True',
             },
-    
+
           ],
         )
 
-def get_auto_scaling_groups_names(ASG_client) : 
+def get_auto_scaling_groups_names(ASG_client) :
     ASG_names_list = []
     respone_name = ASG_client.describe_auto_scaling_groups()
     all_groups = respone_name['AutoScalingGroups']
     for i in all_groups:
         ASG_names_list.append(i['AutoScalingGroupName'])
     return ASG_names_list
-    
+
 
 def get_all_region_names():
     client = boto3.client('ec2')
@@ -63,28 +62,24 @@ def update_tag_manual_ASG():
             list_all_ASG_names = (get_auto_scaling_groups_names(ASG_client))
             if check_scaling_policies(list_all_ASG_names , ASG_client):
                 list_manual_ASG_names = check_scaling_policies(list_all_ASG_names , ASG_client)
-                print("{0} : {1}".format(region ,list_manual_ASG_names))
-                logging.info("{0} : {1}".format(region ,list_manual_ASG_names)
+                list_tag_manual_ASG_names = "{0} : {1}".format(region ,list_manual_ASG_names)
+                send_sns_asg_tag_groups(SNS_REGION , SNS_ARN_TOPIC,list_manual_ASG_names)
+                print(list_tag_manual_ASG_names) # log to cloudwatch
                 update_tags(list_manual_ASG_names, ASG_client)
 
 
+def send_sns_asg_tag_groups(region, topic_arn , list_tag_manual_ASG_names):
+    sns_client = boto3.client('sns', region_name=region)
+    message = "The lambda tag manual ASG: " +  ', '.join(list_tag_manual_ASG_names) + "\n for more information you can enter to CloudWatch: https://" +SNS_REGION+ ".console.aws.amazon.com/cloudwatch/home "
+    response = sns_client.publish(
+        TopicArn = topic_arn,
+        Message = message
+    )
+
 def lambda_handler(event, context):
     update_tag_manual_ASG()
-    # list_manual_ASG_names = []
-    # list_region = []
-    # all_region = get_all_region_names()
-    # for region in all_region :
-    #   ASG_client = boto3.client('autoscaling', region)
-    #   if get_auto_scaling_groups_names(ASG_client):
-    #       list_all_ASG_names = (get_auto_scaling_groups_names(ASG_client))
-    #       if check_scaling_policies(list_all_ASG_names , ASG_client):
-    #           list_region.append(region)
-    #           list_manual_ASG_names.append(check_scaling_policies(list_all_ASG_names , ASG_client))
-    # print(dict(zip(list_region, list_manual_ASG_names)))           
 
-    
+
 
 if __name__ == "__main__":
     main()
-    
-# TODO : LOGS to cloudwatch
